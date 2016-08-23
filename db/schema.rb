fc524aa53879f7002aa76b80bc37ffddb3e365f7
rango905 <wuweiwei.chn@gmail.com>
2016-08-23 13:54:56 +0800

    Create Cart and db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819084617) do
+ActiveRecord::Schema.define(version: 20160823055352) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
