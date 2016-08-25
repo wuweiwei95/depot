049ea8b83aba7ca336474aef162c44fd4258fb19
rango905 <wuweiwei.chn@gmail.com>
2016-08-25 14:54:17 +0800

    Add cart button
    1 . add cart button in products page
    2. add cart view template

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -1,48 +1,57 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 require 'test_helper'
 
-class LineItemsControllerTest < ActionDispatch::IntegrationTest
+class LineItemsControllerTest < ActionController::TestCase
   setup do
     @line_item = line_items(:one)
   end
 
   test "should get index" do
-    get line_items_url
+    get :index
     assert_response :success
+    assert_not_nil assigns(:line_items)
   end
 
   test "should get new" do
-    get new_line_item_url
+    get :new
     assert_response :success
   end
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+      post :create, product_id: products(:ruby).id
     end
 
-    assert_redirected_to line_item_url(LineItem.last)
+    assert_redirected_to cart_path(assigns(:line_item).cart)
   end
 
   test "should show line_item" do
-    get line_item_url(@line_item)
+    get :show, id: @line_item
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_line_item_url(@line_item)
+    get :edit, id: @line_item
     assert_response :success
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
-    assert_redirected_to line_item_url(@line_item)
+    patch :update, id: @line_item, line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id }
+    assert_redirected_to line_item_path(assigns(:line_item))
   end
 
   test "should destroy line_item" do
     assert_difference('LineItem.count', -1) do
-      delete line_item_url(@line_item)
+      delete :destroy, id: @line_item
     end
 
-    assert_redirected_to line_items_url
+    assert_redirected_to line_items_path
   end
 end
